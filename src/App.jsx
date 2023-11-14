74e9380b2ea6f133b1881435e84e7b782c511024
ervinong <137969593+ervinong@users.noreply.github.com>
2023-11-14 23:45:00 +0000

    set hompeage for github

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ervin Ong",
+  title: "Data Analyst & Software Engineer",
+  email: "E0725470@U.NUS.EDU",
+  gitHub: "ervinong",
+  instagram: "",
+  linkedIn: "ervin-ong-69657b154",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "UCqG4Az6tXKj_R7htd6S2h4w",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/24.09.18.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "campfire";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Hera Solutions' Admin Panel",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Designed and developed a dashboard for a startup based in Trondheim, Norway. The website is built with React and deployed on Vercel.",
+    url: "https://admin.herasolutions.no/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Namer App",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Created a mobile app that generates random names for your next project. The app is built with Flutter and deployed to Github pages.",
+    url: "https://ervinong.github.io/namer_app/#/",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+      "Created from Github's Javascript Codespaces (React). Includes my experiences and interests.",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Art and Animation Projects",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Created a series of drawings and animations. The projects are built with Adobe Photoshop, GIMP and Blender.",
+    url: "https://www.instagram.com/early.archive",
   },
 ];
 
