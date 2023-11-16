99bf5757218f5cfe8a8f0d198484a29d97bf3d9c
ervinong <137969593+ervinong@users.noreply.github.com>
2023-11-16 12:32:20 +0000

    smooth scrolling

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -39,17 +39,17 @@ const projectList = [
   {
     title: "Namer App",
     description:
-      "Created a mobile app that generates random names for your next idea. The app is built with Flutter and deployed to Github pages.",
+      "Created a mobile app that generates random names for your next idea. The app is built with Flutter and deployed to Github.",
     url: "https://ervinong.github.io/namer_app/#/",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Github's Javascript Codespaces (React). Includes my experiences and interests.",
+      "Created from Github's Javascript Codespaces (React). This site documents my experiences and interests.",
     url: "http://ervinong.github.io/my-portfolio",
   },
   {
-    title: "Art & Animation Projects",
+    title: "Art/Animation Projects",
     description:
       "Created a series of drawings and animations. The projects are built with Adobe Photoshop, GIMP and Blender.",
     url: "https://www.instagram.com/early.archive",
